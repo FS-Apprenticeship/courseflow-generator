9934e7fef871e10f00d68b6e5ec1e83e21fe6d72
itsKia2 <omerkarimi29@gmail.com>
2025-11-12 16:32:51 -0500

    added functions to handle uploading, getting, deleting courses from db

--- src/stores/course.js
+++ src/stores/course.js
@@ -6,23 +6,34 @@
 import { ref, reactive } from 'vue'
 
 import { defineStore } from 'pinia'
-import { createOverview, createCourse, refineCourse } from '@/services/openai';
+import { supa } from '@/services/auth'
+import { createOverview, createCourse, refineCourse } from '@/services/openai'
+import { dbGetCourses, dbUploadCourses, dbUpdateCourse, dbDeleteCourse } from '@/services/dbCourses'
 
-export const useCourseStore = defineStore("courseStore", () => {
-  const user = ref(null);
-  const course = ref({});
+export const useCourseStore = defineStore('courseStore', () => {
+  const user = ref(null)
+  const course = ref({})
   const overview = reactive({
     overview: null,
     num_lessons: null,
-    total_hours: null
-  });
+    total_hours: null,
+  })
+
+  const course_id = ref(null);
 
   // for router guards
-  const selectionCompleted = ref(false);
-  const overviewCompleted = ref(false);
+  const selectionCompleted = ref(false)
+  const overviewCompleted = ref(false)
 
   async function aiCreateOverview(goal, duration, profile) {
-    let data = await createOverview(goal, duration, profile.age, profile.reading_level, profile.interests, profile.learning_style);
+    let data = await createOverview(
+      goal,
+      duration,
+      profile.age,
+      profile.reading_level,
+      profile.interests,
+      profile.learning_style,
+    )
     data = JSON.parse(data.text)
     this.overview.overview = data.overview
     this.overview.num_lessons = data.num_lessons
@@ -30,7 +41,15 @@ export const useCourseStore = defineStore("courseStore", () => {
   }
 
   async function aiCreateCourse(profile) {
-    let data = await createCourse(overview.overview, overview.total_hours, overview.num_lessons, profile.age, profile.reading_level, profile.interests, profile.learning_style);
+    let data = await createCourse(
+      overview.overview,
+      overview.total_hours,
+      overview.num_lessons,
+      profile.age,
+      profile.reading_level,
+      profile.interests,
+      profile.learning_style,
+    )
     data = JSON.parse(data.text)
     course.value = data
   }
@@ -41,10 +60,48 @@ export const useCourseStore = defineStore("courseStore", () => {
       data = JSON.parse(data.text)
       course.value = data
     } catch (error) {
-      console.log("Error refining course:", error)
+      console.log('Error refining course:', error)
       throw error
     }
   }
 
-  return { user, course, overview, selectionCompleted, overviewCompleted, aiCreateOverview, aiCreateCourse, aiRefineCourse }
+  async function uploadCourse() {
+    if (course.value == {}) {
+      console.log("course not found - cant upload")
+      return;
+    }
+
+    const data = await dbUploadCourses(supa, user.value.id, course.value);
+    course_id.value = data.id;
+    return data;
+  }
+
+  async function getCourses() {
+    const data = await dbGetCourses(supa, user.value.id);
+    return data;
+  }
+
+  async function updateCourse() {
+    const data = await dbUpdateCourse(supa, course_id.value, course.value);
+    return data;
+  }
+
+  async function deleteCourse(id) {
+    await dbDeleteCourse(supa, id)
+  }
+
+  return {
+    user,
+    course,
+    overview,
+    selectionCompleted,
+    overviewCompleted,
+    aiCreateOverview,
+    aiCreateCourse,
+    aiRefineCourse,
+    uploadCourse,
+    getCourses,
+    updateCourse,
+    deleteCourse,
+  }
 })
