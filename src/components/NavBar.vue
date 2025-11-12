cf67101ebdce9e0b4717b9c2d99807fe201cd8f8
itsKia2 <omerkarimi29@gmail.com>
2025-11-12 17:58:58 -0500

    added button for back to selection

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useUserStore } from '@/stores/user'
 import router from '@/router'
+import { useRoute } from 'vue-router'
 
 const userStore = useUserStore()
 
 // function for WHEN to display the sign out button
 const showSignOut = userStore.isLoggedIn
+
+// check when to show back to selection button
+const pathsForBack = ["/course", "/overview", "/saved-courses"]
+const route = useRoute()
+const showBackButton = pathsForBack.includes(route.path)
 
 // helper function to actually sign the person out
 async function signOutHelper() {
@@ -14,19 +20,24 @@ async function signOutHelper() {
     window.location.reload()
   })
 }
+
+function goBackToSelection() {
+  router.push('/selection')
+}
 </script>
 
 <template>
-  <nav
-    class="bg-gray-900 border-b border-gray-800 text-white px-6 py-4 flex justify-between items-center"
-  >
+  <nav class="bg-gray-900 border-b border-gray-800 text-white px-6 py-4 flex justify-between items-center">
     <div class="font-bold text-xl">Course Flow Generator - Flatiron School</div>
-    <button
-      v-if="showSignOut"
-      @click="signOutHelper()"
-      class="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 border border-gray-700 transition-colors font-medium"
-    >
-      Sign Out
-    </button>
+    <div class="flex gap-4 items-center">
+      <button v-if="showBackButton" @click="goBackToSelection()"
+        class="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 border border-gray-700 transition-colors font-medium">
+        Back to Selection
+      </button>
+      <button v-if="showSignOut" @click="signOutHelper()"
+        class="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 border border-gray-700 transition-colors font-medium">
+        Sign Out
+      </button>
+    </div>
   </nav>
 </template>
